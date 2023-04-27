311c38a8a46d2646a3d5f5d9350fabacd0739c77
Shayne M <nshayne.mcpherson@gmail.com>
2023-04-27 11:47:24 -0500

    initial commit

---
+++ src/test/java/com/personal/projects/MusicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
